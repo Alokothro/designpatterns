b0d5adfeb62afb5009c81009b51c91aa0df10d18
Alok Patel <alokothro@gmail.com>
2024-09-25 21:29:31 -0400

    Iterator

--- Iterator/Flight.java
+++ Iterator/Flight.java
@@ -1,6 +1,13 @@
 package Iterator;
+
 import java.time.LocalTime;
 
+/**
+ * The Flight class represents a flight with a flight number, departure and arrival airports, 
+ * start and end times, and the number of transfers.
+ * 
+ * @author Alok Patel
+ */
 public class Flight {
     private String flightNum;
     private Airport from;
@@ -9,6 +16,17 @@ public class Flight {
     private LocalTime endTime;
     private int numTransfers;
 
+    /**
+     * Constructs a Flight object with the specified flight number, origin, destination, 
+     * start time, end time, and the number of transfers.
+     * 
+     * @param flightNum The flight number.
+     * @param from The departure airport.
+     * @param to The arrival airport.
+     * @param startTime The time the flight departs.
+     * @param endTime The time the flight arrives.
+     * @param numTransfers The number of transfers for the flight.
+     */
     public Flight(String flightNum, Airport from, Airport to, LocalTime startTime, LocalTime endTime, int numTransfers) {
         this.flightNum = flightNum;
         this.from = from;
@@ -18,18 +36,41 @@ public class Flight {
         this.numTransfers = numTransfers;
     }
 
+    /**
+     * Returns the departure airport of the flight.
+     * 
+     * @return The departure airport.
+     */
     public Airport getFrom() {
         return from;
     }
 
+    /**
+     * Returns the arrival airport of the flight.
+     * 
+     * @return The arrival airport.
+     */
     public Airport getTo() {
         return to;
     }
 
+    /**
+     * Checks if the flight's origin and destination match the provided airports.
+     * 
+     * @param from The departure airport to compare.
+     * @param to The arrival airport to compare.
+     * @return True if both the origin and destination match; false otherwise.
+     */
     public boolean sameLoc(Airport from, Airport to) {
         return this.from == from && this.to == to;
     }
 
+    /**
+     * Returns a string representation of the flight details, including origin, destination, 
+     * departure time, arrival time, duration, and number of transfers.
+     * 
+     * @return A formatted string with flight details.
+     */
     @Override
     public String toString() {
         return String.format("%s to %s %s - %s (%dh %dm) %s",
--- Iterator/FlightIterator.java
+++ Iterator/FlightIterator.java
@@ -1,19 +1,39 @@
 package Iterator;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * The FlightIterator class provides an iterator to filter and iterate over flights
+ * that match a specific origin and destination airport.
+ * 
+ * @author Alok Patel
+ */
 public class FlightIterator implements Iterator<Flight> {
     private ArrayList<Flight> flights;
     private int position = 0;
     private Airport from;
     private Airport to;
 
+    /**
+     * Constructs a FlightIterator for the given list of flights, filtering by origin 
+     * and destination airports.
+     * 
+     * @param flights The list of flights to iterate through.
+     * @param from The origin airport to filter flights by.
+     * @param to The destination airport to filter flights by.
+     */
     public FlightIterator(ArrayList<Flight> flights, Airport from, Airport to) {
         this.flights = flights;
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Checks if there are more flights that match the origin and destination.
+     * 
+     * @return true if a matching flight is found, false otherwise.
+     */
     @Override
     public boolean hasNext() {
         while (position < flights.size()) {
@@ -26,6 +46,11 @@ public class FlightIterator implements Iterator<Flight> {
         return false;
     }
 
+    /**
+     * Returns the next flight that matches the origin and destination airports.
+     * 
+     * @return The next matching Flight object.
+     */
     @Override
     public Flight next() {
         Flight flight = flights.get(position);
